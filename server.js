2df2e3098817fa12f57da12f944e8c26bf562158
artavazdot <sweetpaste3815@outlook.com>
2023-03-06 21:51:03 +0900

    Updated server.js.

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
-end process;
+
